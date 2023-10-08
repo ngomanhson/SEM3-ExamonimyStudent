ef004e4d742fad1e7ce249c020a04338087e36b5
ngomanhson <ngomanhson2004txpt@gmail.com>
2023-10-08 15:39:47 +0700

    Delete Signup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-// import Navbar from "./components/layouts/navbar";
 import Home from "./components/pages/home";
-// import Footer from "./components/layouts/footer";
-// import BackToTop from "./components/layouts/back-top";
 import Course from "./components/pages/course";
 import CourseDetail from "./components/pages/course-details";
 import Contact from "./components/pages/contact";
 import Blog from "./components/pages/blog";
 import BlogDetail from "./components/pages/blog-details";
 import Login from "./components/pages/login";
-import Signup from "./components/pages/signup";
 import MultipleChoice from "./components/pages/multiple-choice";
 import ExamList from "./components/pages/exam-list";
 import ConstructedResponse from "./components/pages/practical-exam";
@@ -22,21 +18,19 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/course" element={<Course />} />
+                <Route path="/course/detail" element={<CourseDetail />} />
 
                 <Route path="/exam-list" element={<ExamList />} />
                 <Route path="/multiple-choice" element={<MultipleChoice />} />
                 <Route path="/practical-exam" element={<ConstructedResponse />} />
 
-                <Route path="/course/detail" element={<CourseDetail />} />
-                <Route path="/contact" element={<Contact />} />
-
                 <Route path="/blog" element={<Blog />} />
                 <Route path="/blog/detail" element={<BlogDetail />} />
 
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/contact" element={<Contact />} />
 
+                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
